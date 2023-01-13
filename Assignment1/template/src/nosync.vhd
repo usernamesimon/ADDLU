fb1000b7169459bb9ee50b6c993d5fe721180c4b
Simon MAYER <s11776870@ecslab7.ecslab.tuwien.ac.at>
2023-01-13 11:46:48 +0100

    Separate architectures for the subtasks

--- Assignment1/template/src/nosync.vhd
+++ Assignment1/template/src/nosync.vhd
@@ -46,7 +46,45 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-architecture src_arch of src is
+architecture nosync of src is
+  signal data_tgl            : std_logic_vector(7 downto 0);
+  attribute keep             : boolean;
+  attribute keep of data_tgl : signal is true;
+  signal data_cnt            : unsigned(3 downto 0);
+  signal reqreg              : std_logic;
+  signal reset_sync          : std_logic_vector(1 downto 0);
+begin
+  rst : process(clk, reset_n)
+  begin
+    if reset_n = '0' then
+      reset_sync <= (others => '0');
+    elsif rising_edge(clk) then
+      reset_sync <= '1' & reset_sync(1);
+    end if;
+  end process rst;
+
+  sync : process(clk, reset_sync)
+  begin
+    if reset_sync(0) = '0' then
+      data_tgl <= "10101010";
+      data_cnt <= to_unsigned(0, data_cnt'length);
+      reqreg   <= '0';
+    elsif rising_edge(clk) then
+      data_tgl <= not data_tgl;
+      data_cnt <= data_cnt + 1;
+      reqreg   <= not reqreg;
+    end if;
+  end process sync;
+
+  data <= data_tgl & std_logic_vector(data_cnt);
+  req  <= reqreg;
+end architecture;
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+architecture flowcontrol of src is
   signal data_tgl            : std_logic_vector(7 downto 0);
   attribute keep             : boolean;
   attribute keep of data_tgl : signal is true;
@@ -85,7 +123,7 @@ begin
   req  <= reqreg;
 end architecture;
 
-architecture dst_arch of dst is
+architecture flowcontrol of dst is
   signal reset_sync : std_logic_vector(1 downto 0);
   signal datareg    : std_logic_vector(data'range);
   signal oldreq     : std_logic;
@@ -119,12 +157,43 @@ begin
   ack <= ackreg;
 end architecture;
 
+architecture nosync of dst is
+  signal reset_sync : std_logic_vector(1 downto 0);
+  signal datareg    : std_logic_vector(data'range);
+  signal oldreq     : std_logic;
+begin
+  rst : process(clk, reset_n)
+  begin
+    if reset_n = '0' then
+      reset_sync <= (others => '0');
+    elsif rising_edge(clk) then
+      reset_sync <= '1' & reset_sync(1);
+    end if;
+  end process rst;
+
+  reg : process(clk, reset_sync, req)
+  begin
+    if reset_sync(0) = '0' then
+      datareg <= (others => '0');
+      oldreq   <= '0';
+		datareg  <=  (others => '0');
+    elsif rising_edge(clk) then
+      oldreq <= req;
+      if oldreq /= req then
+        datareg <= data;
+      end if;
+    end if;
+  end process reg;
+
+  data_out <= datareg;
+end architecture;
+
 architecture top_arch of top is
   signal s_data : std_logic_vector(11 downto 0);
   signal s_req  : std_logic;
   signal s_ack  : std_logic;
 begin
-  src_inst : entity work.src
+  src_inst : entity work.src(nosync)
     port map (
       clk     => clk_src,
       reset_n => reset_n,
@@ -133,7 +202,7 @@ begin
       ack     => s_ack
       );
 
-  dst_inst : entity work.dst
+  dst_inst : entity work.dst(nosync)
     port map (
       clk      => clk_dst,
       reset_n  => reset_n,
